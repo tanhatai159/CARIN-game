ba9cee23eb925174f58a833f63be810d1d7718b9
DifficultIV <kitpisan@gmail.com>
2022-02-14 16:17:52 +0700

    Add Tokenizer

---
+++ src/Parser/Tokenizer.java
@@ -0,0 +1,51 @@
+package Parser;
+
+import java.util.LinkedList;
+
+public class Tokenizer {
+    LinkedList<String> parse;
+
+    public Tokenizer(String s) {
+        genarate(s);
+        System.out.println(parse);
+    }
+
+    public String consume() {
+        return parse.poll();
+    }
+    public boolean hasNext(){
+        return parse.isEmpty();
+    }
+    public String Next(){
+        return parse.poll();
+    }
+
+    public void Print(){
+        System.out.println(parse);
+    }
+    public void genarate(String input) {
+        parse = new LinkedList<String>();
+        String line = input;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < line.length(); i++) {
+//            if(line.charAt(i) == ' ') continue;
+
+                if (line.charAt(i) == ' ') {
+                    if(sb.isEmpty()) continue;
+                    this.parse.add(sb.toString());
+                    sb = new StringBuilder();
+                }else{
+                    sb.append(line.charAt(i));
+                }
+
+        }
+        if (sb.length() > 0) this.parse.add(sb.toString());
+    }
+
+    public static void main(String[] args) {
+        Tokenizer a = new Tokenizer("Flan Laplus Callie");
+        String X = a.consume();
+        a.Print();
+        System.out.println(X);
+    }
+}
