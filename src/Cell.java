20b0f71c8ebb417e1f781200e7901afd24ea54a0
TunyakarnK <tunyakarn2544@gmail.com>
2022-02-14 22:46:10 +0700

    implement move and shoot

--- src/Cell.java
+++ src/Cell.java
@@ -1,6 +1,65 @@
+
 public class Cell {
     protected final String[] element = {"fire", "water", "glass"};
     String type; // type of each cell
     Organ currentOrgan; // เพื่อบอกว่า cell ตัวนี้อยู่ที่ organ ไหน
     int x, y;
+    int hp,dmg;
+
+    public void move(String direction){
+        switch (direction) {
+            case "left" -> x = x - 1;
+            case "right" -> x = x + 1;
+            case "up" -> y = y - 1;
+            case "down" -> y = y + 1;
+            case "upleft" -> {
+                x = x - 1;
+                y = y - 1;
+            }case "upright" -> {
+                x = x + 1;
+                y = y - 1;
+            }case "downleft" -> {
+                x = x - 1;
+                y = y + 1;
+            }case "downright" -> {
+                x = x + 1;
+                y = y + 1;
+            }
+        }
+    }
+
+
+    public void shoot(String direction){
+        int xEnemy = x, yEnemy = y;
+        switch (direction) {
+            case "left" -> xEnemy = x - 1;
+            case "right" -> xEnemy = x + 1;
+            case "up" -> yEnemy = y - 1;
+            case "down" -> yEnemy = y + 1;
+            case "upleft" -> {
+                xEnemy = x - 1;
+                yEnemy = y - 1;
+            }case "upright" -> {
+                xEnemy = x + 1;
+                yEnemy = y - 1;
+            }case "downleft" -> {
+                xEnemy = x - 1;
+                yEnemy = y + 1;
+            }case "downright" -> {
+                xEnemy = x + 1;
+                yEnemy = y + 1;
+            }
+        }
+        Cell enemy = currentOrgan.coordinate(xEnemy,yEnemy);
+        int enemyHP = enemy.hp;
+        if (enemy instanceof Antibody){
+            ((Antibody) enemy).attackByThisVirus = (Virus) this;
+        }
+        enemyHP = enemyHP - this.dmg;
+    }
+
+//    public int virusScan(){}
+//    public int antibodyScan(){}
+//    public int nearby(){}
+
 }
