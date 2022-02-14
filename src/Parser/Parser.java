850d937fe81c1203204859aaceab5deb11a5f928
tanhatai159 <gun159@outlook.com>
2022-02-14 21:03:35 +0700

    add all Parser method

--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,12 +1,59 @@
 package Parser;
 
+import Node.ProgramNode;
+
 public class Parser {
     Tokenizer AllToken;
     public Parser(String path){
         AllToken = new Tokenizer(path);
     }
 
-    public void ParesProgram(){
+    public void Pares(){
+
+    }
+    public void ParseProgram(){
+
+    }
+    public void ParseStatement(){
+
+    }
+    public void ParseCommand(){
+
+    }
+    public void ParseAssignmentStatement(){
+
+    }
+    public void ParseActionCommand(){
+
+    }
+    public void ParseMoveCommand(){
+
+    }
+    public void ParseAttackCommand(){
+
+    }
+    public void ParseBlockStatement(){
+
+    }
+    public void ParseIfStatement(){
+
+    }
+    public void ParseWhileStatement(){
+
+    }
+    public void ParseExpression(){
+
+    }
+    public void ParseTerm(){
+
+    }
+    public void ParseFactor(){
+
+    }
+    public void ParsePower(){
+
+    }
+    public void ParseSensorExpression(){
 
     }
 }
