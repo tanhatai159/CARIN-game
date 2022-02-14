cee050892de2510f71b17077801480d2ccb70e35
tanhatai159 <gun159@outlook.com>
2022-02-14 22:19:43 +0700

    implementing Parser

--- src/Node/ProgramNode.java
+++ src/Node/ProgramNode.java
@@ -1,6 +1,11 @@
 package Node;
 
-public class ProgramNode {
-    StatementNode statement;
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class ProgramNode implements Program {
+    Queue<StatementNode> q = new LinkedList<>();
+    public void addStatement(StatementNode statement){
+        q.add(statement);
+    }
 }
--- src/Parser/Parser.java
+++ src/Parser/Parser.java
@@ -1,21 +1,39 @@
 package Parser;
 
 import Node.ProgramNode;
+import Node.StatementNode;
+
+import java.beans.Statement;
 
 public class Parser {
     Tokenizer AllToken;
+
+    private boolean isCommand(String word){
+        return word.equals("move") || word.equals("shoot");
+    }
+
     public Parser(String path){
         AllToken = new Tokenizer(path);
     }
 
-    public void Pares(){
+    public void Parse(){
 
     }
     public void ParseProgram(){
-
+        ProgramNode p = new ProgramNode();
+        while (AllToken.hasNext()){
+            p.addStatement(ParseStatement());
+        }
     }
-    public void ParseStatement(){
+    public StatementNode ParseStatement(){
 
+        if(isCommand(AllToken.peek())){
+
+        }
+        else if(AllToken.peek().equals("if") || AllToken.peek().equals("else")){
+
+        }
+        return null;
     }
     public void ParseCommand(){
 
