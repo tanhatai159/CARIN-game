feaa89bf9603192bc03da540494326a6cc9139a8
tanhatai159 <gun159@outlook.com>
2022-02-18 21:24:32 +0700

    improve MainGame and Player

--- src/MainClass/MainGame.java
+++ src/MainClass/MainGame.java
@@ -2,10 +2,14 @@ package MainClass;
 
 public class MainGame {
     private static int timeUnit = 0;
+
     public static int getTimeUnit(){
         return timeUnit;
     }
     public static void setTimeUnit(int n){
         timeUnit = n;
+    }
+    public static void increaseTimeUnit(int amount){
+        timeUnit += amount;
     }
 }
--- src/MainClass/Player.java
+++ src/MainClass/Player.java
@@ -1,10 +1,21 @@
 package MainClass;
 
 public class Player {
-    int credits;
-    static int initialCredits, time = 0;
+    private static int credits;
 
     public static void update(int credits){
-        Player.initialCredits = credits;
+        Player.credits = credits;
+    }
+
+    public static int getCredits() {
+        return credits;
+    }
+
+    public static void increaseCredit(int amount){
+        credits += amount;
+    }
+
+    public static void decreaseCredit(int amount){
+        credits -= amount;
     }
 }
