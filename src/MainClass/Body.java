8d9dccc4e96ff37c02b2397a4968656396d21c60
tanhatai159 <gun159@outlook.com>
2022-02-18 23:23:43 +0700

    improve Body

--- src/MainClass/Body.java
+++ src/MainClass/Body.java
@@ -1,5 +1,13 @@
 package MainClass;
 
-public class Body {
+import java.util.ArrayList;
 
+public class Body {
+    static ArrayList<Organ> AllOrgan;
+
+    public static void update(){
+        for(int i = 0;i < 3;i++){
+            AllOrgan.add(new Organ());
+        }
+    }
 }
