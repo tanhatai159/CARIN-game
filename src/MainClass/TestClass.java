a3007a8fec4aefc7a448ba6198f7b2110ebed396
tanhatai159 <gun159@outlook.com>
2022-02-18 21:54:08 +0700

    add TestClass for test

--- src/MainClass/TestClass.java
+++ src/MainClass/TestClass.java
@@ -4,9 +4,12 @@ import Node.ProgramNode;
 import Parser.Parser;
 
 public class TestClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Parser parser = new Parser("D:\\code\\year2\\OOP\\GPS\\testCase\\forReadConfig\\ttttt");
+//        Organ testOrgan = new Organ();
+//        ReadConfig.readConfig();
+//        testOrgan.generateVirus();
         ProgramNode p = parser.Parse();
-        System.out.println("5555");
+        System.out.println(MainGame.getTimeUnit());
     }
 }
