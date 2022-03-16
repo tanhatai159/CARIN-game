046884e7613dd53479812eef07626699e4077bd9
TunyakarnK <tunyakarn2544@gmail.com>
2022-03-16 14:03:59 +0700

    update cellpanel

--- src/Graphic/CellPanel.java
+++ src/Graphic/CellPanel.java
@@ -1,7 +1,26 @@
 package Graphic;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class CellPanel extends JPanel {
+    private int cellPanelWidth = 900,cellPanelHeight = 500;
+    private GridPanel grid = new GridPanel();
 
+    public CellPanel(){
+        setOpaque(true);
+        this.setLayout(null);
+        this.setBounds(0,220,cellPanelWidth,cellPanelHeight);
+        this.setBackground(Color.white);
+    }
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics g2D = (Graphics2D) g;
+
+        add(grid);
+        grid.setBounds(0,220,900,500);
+    }
 }
+
--- src/Graphic/GameScreen.java
+++ src/Graphic/GameScreen.java
@@ -59,6 +59,154 @@ public class GameScreen extends JPanel {
         this.add(new CellPanel());
     }
     public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        Graphics g2D = (Graphics2D) g;
+
+//        //first Rec
+//        g2D.setColor(new Color(151,52,46));
+//        g2D.fillRect(0,0,firstRecWidth,firstRecHeight);
+//
+//        //shop Rec
+//        g2D.setColor(new Color(222,156,151));
+//        g2D.fillRect(firstRecWidth,0,shopRecWidth,shopRecHeight);
+
+        //background Rec
+        g2D.setColor(new Color(193,82,75));
+        g2D.fillRect(0,firstRecHeight,backgroundWidth,backgroundHeight);
+
+        //amount of antibody's text on first Rec
+        Font font = new Font("Roboto Condensed",Font.PLAIN,36);
+//        g2D.setFont(font);
+        FontMetrics fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("Antibody: ",antiPositionText_X,antiPositionText_Y+fontMetrics.getAscent());
+//        g2D.drawString(String.valueOf(Antibody.getAmountOfAntibody()),amountOfAntiPositionText_X,amountOfAntiPositionText_Y + fontMetrics.getAscent());
+//
+//        //amount of virus's text on first Rec
+//        font = new Font("Roboto Condensed",Font.PLAIN,36);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("Virus: ",virusPositionText_X,virusPositionText_Y+fontMetrics.getAscent());
+//        g2D.drawString(String.valueOf(Virus.getAmountOfVirus()),amountOfVirusPositionText_X,amountOfVirusPositionText_Y + fontMetrics.getAscent());
+
+//        //CARIN header on Shop Rec
+//        font = new Font("Roboto Condensed",Font.PLAIN,80);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("CARIN",carinPositionText_X,carinPositionText_Y+fontMetrics.getAscent());
+//
+//        //element image
+//        this.add(element);
+//        element.setBounds(elementPosition_X,elementPosition_Y,100,100);
+//
+//        //Credits text
+//        font = new Font("Roboto Condensed",Font.PLAIN,36);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("Credits:",creditPositionText_X,creditPositionText_Y+fontMetrics.getAscent());
+//
+//        //CreditAmount Text
+//        font = new Font("Roboto Condensed",Font.PLAIN,36);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString(String.valueOf(Player.getCredits() + "$"),amountOfCreditPositionText_X,amountOfCreditPositionText_Y + fontMetrics.getAscent());
+//
+//        //cost redAntibody
+//        font = new Font("Roboto Condensed",Font.PLAIN,24);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("COST: " + String.valueOf(Antibody.getPlacementCost())+ "$",costRedPositionAnti_X,costRedPositionAnti_Y+fontMetrics.getAscent());
+//
+//        //cost greenAntibody
+//        font = new Font("Roboto Condensed",Font.PLAIN,24);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("COST: " + String.valueOf(Antibody.getPlacementCost()) + "$",costGreenPositionAnti_X,costGreenPositionAnti_Y+fontMetrics.getAscent());
+//
+//        //cost blueAntibody
+//        font = new Font("Roboto Condensed",Font.PLAIN,24);
+//        g2D.setFont(font);
+//        fontMetrics = g2D.getFontMetrics();
+//        g2D.setColor(Color.white);
+//        g2D.drawString("COST: " + String.valueOf(Antibody.getPlacementCost() + "$"),costBluePositionAnti_X,costBluePositionAnti_Y+fontMetrics.getAscent());
+
+    }
+
+    public void createButtonForFirstRec(){
+        //brain button
+        brainButton = new JButton("BRAIN");
+        brainButton.setBackground(new Color(222,156,151));
+        brainButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        brainButton.setBounds(brainButtonPosition_X,brainButtonPosition_Y,trioButtonWidth,trioButtonHeight);
+
+        //heart button
+        heartButton = new JButton("HEART");
+        heartButton.setBackground(new Color(222,156,151));
+        heartButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        heartButton.setBounds(heartButtonPosition_X,heartButtonPosition_Y,trioButtonWidth,trioButtonHeight);
+
+        //liver button
+        liverButton = new JButton("LIVER");
+        liverButton.setBackground(new Color(222,156,151));
+        liverButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        liverButton.setBounds(liverButtonPosition_X,liverButtonPosition_Y,trioButtonWidth,trioButtonHeight);
+
+        //AntivirusRed button
+        antiRedButton = new JButton(antiRed);
+        antiRedButton.setBackground(new Color(240,191,187));
+        antiRedButton.setBounds(antiRedButtonPosition_X,antiRedButtonPosition_Y,antibodyButtonWidth,antibodyButtonHeight);
+
+        //AntivirusGreen button
+        antiGreenButton = new JButton(antiGreen);
+        antiGreenButton.setBackground(new Color(240,191,187));
+        antiGreenButton.setBounds(antiGreenButtonPosition_X,antiGreenButtonPosition_Y,antibodyButtonWidth,antibodyButtonHeight);
+
+        //AntivirusBlue button
+        antiBlueButton = new JButton(antiBlue);
+        antiBlueButton.setBackground(new Color(240,191,187));
+        antiBlueButton.setBounds(antiBlueButtonPosition_X,antiBlueButtonPosition_Y,antibodyButtonWidth,antibodyButtonHeight);
+
+        // buyButton
+        buyButton = new JButton("BUY");
+        buyButton.setForeground(Color.white);
+        buyButton.setBackground(new Color(193,82,75));
+        buyButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        buyButton.setBounds(buyButtonPosition_X,buyButtonPosition_Y,trioButtonWidth,trioButtonHeight);
+
+        //doneButton
+        doneButton = new JButton("DONE");
+        doneButton.setForeground(Color.white);
+        doneButton.setBackground(new Color(193,82,75));
+        doneButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        doneButton.setBounds(doneButtonPosition_X,doneButtonPosition_Y,trioButtonWidth,trioButtonHeight);
+
+        //exitButton
+        exitButton = new JButton("EXIT");
+        exitButton.setForeground(Color.white);
+        exitButton.setBackground(new Color(151,52,46));
+        exitButton.setFont(new Font("Roboto Condensed",Font.PLAIN,24));
+        exitButton.setBounds(exitButtonPosition_X,exitButtonPosition_Y,exitButtonWidth,exitButtonHeight);
+        exitButton.addActionListener( new ActionListener()
+        {
+            public void actionPerformed(ActionEvent e)
+            {System.exit(0);}
+        });
+
+        this.add(brainButton);
+        this.add(heartButton);
+        this.add(liverButton);
+        this.add(antiRedButton);
+        this.add(antiGreenButton);
+        this.add(antiBlueButton);
+        this.add(buyButton);
+        this.add(doneButton);
+        this.add(exitButton);
 
     }
 
