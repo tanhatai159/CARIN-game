bceda4b4e7509bf24fb4f22a91cdf2c6262d86e1
tanhatai159 <gun159@outlook.com>
2022-03-16 14:47:09 +0700

    make gui can create cell button

--- src/Graphic/CellButton.java
+++ src/Graphic/CellButton.java
@@ -4,5 +4,7 @@ import javax.swing.*;
 
 public class CellButton extends JButton {
     private boolean isEmpty = true;
+    private boolean isAntibody = false;
+    private boolean isVirus = false;
 
 }
